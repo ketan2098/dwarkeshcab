a502eec5d70778a2d0086c23448a58c62ed2c368
hardik <113549733+ketan2098@users.noreply.github.com>
2026-01-30 23:55:44 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-// import Navbar from "./components/Navbar";
-// import Home from "./Page/Home";
-// import ContactUs from "./Page/ContactUs";
-// import { Routes, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/booking" element={<ContactUs />} />
-//       </Routes>
-//     </>
-//   );
-// }
-
-// export default App;
-
 
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,8 +1,8 @@
 import About from "../components/common/About";
 import Responsive from "../components/common/Banner";
-import FeatureSection from "../components/home/Features";
-import Packages from "../components/home/Packages";
-import TaxiCards from "../components/home/TaxiCards";
+import FeatureSection from "../components/homepage/Features";
+import Packages from "../components/homepage/Packages";
+import TaxiCards from "../components/homepage/TaxiCards";
 
 export default function Home() {
     return (
