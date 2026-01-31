2d4042f48af6a7db5a31f653f0a0aa2bc2954e5f
hardik <113549733+ketan2098@users.noreply.github.com>
2026-01-31 10:55:31 +0530

    header bug fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
